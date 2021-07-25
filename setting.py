35dc65a095861618f1f4289e154b8531424490e0
xzqi <xiazeqi@126.com>
2021-07-25 22:13:16 +0800

    infor init

---
+++ setting.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/19 下午8:54
+# @Author  : 'Xiazeqi'
+# 以下为代码区
+# --------------------------------------------------------
+"""
+web frame 配置文件
+"""
+
+# [frame ip]
+frame_ip = '127.0.0.1'
+frame_port = 8080
+
+# [debug]
+DEBUG = True
+
+# [save html pages]
+STATIC_DIR = "./static"
---
+++ urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/19 下午8:54
+# @Author  : 'Xiazeqi'
+# 以下为代码区
+# --------------------------------------------------------
+
+from socket import *
+import json
+from settings import *
+from select import select
+from urls import *
+
